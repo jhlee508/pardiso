b22bf48591c3edd27be41610e49d6dc575ef3d04
Jaehwan Lee <chrisjae508@gmail.com>
2023-08-02 04:59:58 +0000

    compile bash script

--- PanuaPardiso/compile.sh
+++ PanuaPardiso/compile.sh
@@ -1,10 +1,7 @@
+#! /bin/sh
+
 # Pardiso Compile with LAPACK and BLAS
 #g++ pardiso_unsym.cpp -o pardiso_unsym -Llib -lpardiso -llapack -lblas -lgfortran -fopenmp -lpthread -lm
 
 # Pardiso Compile with LAPACK and BLAS + CNpy (Numpy in C++)
 g++ pardiso_samsung.cpp -o pardiso -Lcnpy/lib -lcnpy -lz --std=c++11 -Llib -lpardiso -llapack -lblas -lgfortran -fopenmp -lpthread -lm
-
-# Library location and ETC
-export LD_LIBRARY_PATH=/home/n4/jaehwan/pardiso/panua-pardiso/lib:$LD_LIBRARY_PATH
-export OMP_NUM_THREADS=32
-export PARDISOLICMESSAGE=1
