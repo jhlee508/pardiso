af0ec5d0043c8abc48b87152b2113142f5add6b2
Jaehwan Lee <chrisjae508@gmail.com>
2023-08-07 06:19:13 +0000

    new license for a4 machine

--- PanuaPardiso/compile.sh
+++ PanuaPardiso/compile.sh
@@ -4,4 +4,6 @@
 #g++ pardiso_unsym.cpp -o pardiso_unsym -Llib -lpardiso -llapack -lblas -lgfortran -fopenmp -lpthread -lm
 
 # Pardiso Compile with LAPACK and BLAS + CNpy (Numpy in C++)
-g++ pardiso_samsung.cpp -o pardiso -Lcnpy/lib -lcnpy -lz --std=c++11 -Llib -lpardiso -llapack -lblas -lgfortran -fopenmp -lpthread -lm
+g++ pardiso_samsung.cpp -o pardiso_a4   \
+    -Lcnpy/lib -lcnpy -lz --std=c++11 \
+    -Llib_a4 -lpardiso -llapack -lblas -lgfortran -fopenmp -lpthread -lm
--- PanuaPardiso/setup.sh
+++ PanuaPardiso/setup.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 # Library location and ETC
-export LD_LIBRARY_PATH=/home/n4/jaehwan/pardiso/PanuaPardiso/lib:$LD_LIBRARY_PATH # Pardiso
+export LD_LIBRARY_PATH=/home/n4/jaehwan/pardiso/PanuaPardiso/lib_a4:$LD_LIBRARY_PATH # Pardiso
 export LD_LIBRARY_PATH=/home/n4/jaehwan/pardiso/PanuaPardiso/cnpy/lib:$LD_LIBRARY_PATH # CNPY
-export OMP_NUM_THREADS=32
-export PARDISOLICMESSAGE=1
+
+# Panua Setting
+export OMP_NUM_THREADS=16 # OpenMP Thread Numbers
+export PARDISOLICMESSAGE=1 # Panua License Message Invalidate
+export PARDISO_LIC_PATH=/home/n4/jaehwan/pardiso/PanuaPardiso/license # Panua Pardiso License
